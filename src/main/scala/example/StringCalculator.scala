d28bc3c9f85ab1a969cd402a3c091bda5d1a362c
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-02-21 20:14:20 +0000

    fixed mkString to seperate values with a comma

--- src/main/scala/example/StringCalculator.scala
+++ src/main/scala/example/StringCalculator.scala
@@ -3,7 +3,6 @@ package example
 
 object StringCalculator extends App {
 
-
     def add(numbers: String): Int = {
       if (numbers == "") {
         0
@@ -11,12 +10,13 @@ object StringCalculator extends App {
         val pattern = "[0-9]+".r
         val negs = "[-][0-9]+".r
         val negList = negs.findAllIn(numbers).toList
+
         negList.length match {
           case 0 =>
             val y = pattern.findAllIn(numbers).toList
             y.map(_.toInt).sum
           case _ =>
-            throw new IllegalArgumentException(s"Requirement failed: ${negList.mkString}")
+            throw new IllegalArgumentException(s"Requirement failed: ${negList.mkString(", ")}")
         }
       }
     }
