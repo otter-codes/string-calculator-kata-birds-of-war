c9f4c01933192c095f2dd6165f15ce42ef0d377f
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-02-21 20:26:13 +0000

    refactored to remove unnecessary if statement

--- src/main/scala/example/StringCalculator.scala
+++ src/main/scala/example/StringCalculator.scala
@@ -1,23 +1,20 @@
 package example
 
-
 object StringCalculator extends App {
 
-    def add(numbers: String): Int = {
-      if (numbers == "") {
-        0
-      } else {
-        val pattern = "[0-9]+".r
-        val negs = "[-][0-9]+".r
-        val negList = negs.findAllIn(numbers).toList
+  def add(numbers: String): Int = {
+    val pattern = "[0-9]+".r
+    val negs = "[-][0-9]+".r
+    val negList = negs.findAllIn(numbers).toList
 
-        negList.length match {
-          case 0 =>
-            val y = pattern.findAllIn(numbers).toList
-            y.map(_.toInt).sum
-          case _ =>
-            throw new IllegalArgumentException(s"Requirement failed: ${negList.mkString(", ")}")
-        }
-      }
+    negList.length match {
+      case 0 =>
+        val y = pattern.findAllIn(numbers).toList
+        y.map(_.toInt).sum
+      case _ =>
+        throw new IllegalArgumentException(s"Requirement failed: ${negList.mkString(", ")}")
     }
+  }
 }
+
+
